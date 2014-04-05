ccc274d471d2c4d07811270fba3245bae6634126
Lcf.vs <lcf.vs@hotmail.com>
2014-04-06 01:31:46 +0200

    noexit.js conditionnal patch for root

--- noexit.js
+++ noexit.js
@@ -1,3 +1,8 @@
-process.exit = require('util').deprecate(
+// conditionnal patch for root because that user may need
+// exit codes, e.g., for init.d scripts
+
+if (process.getuid()) {
+  process.exit = require('util').deprecate(
   function() {},
-  'Unwanted process.exit() call prevented by `noexit`')
+  'Unwanted process.exit() call prevented by `noexit`');
+}
